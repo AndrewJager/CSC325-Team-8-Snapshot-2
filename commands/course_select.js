77c27ab7260a0bbabe73c1fe2c528c08916b1624
AndrewJager <35353143+AndrewJager@users.noreply.github.com>
2023-02-16 19:43:25 -0500

    Refactor to work with 1-5 roles

--- commands/course_select.js
+++ commands/course_select.js
@@ -3,60 +3,56 @@ const { SlashCommandBuilder, EmbedBuilder, Embed, PermissionsBitField, ButtonSty
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('courses')
-		.setDescription('assign courses to yourself')
+		.setDescription('Create a message to allow students to assign roles to themselves')
 		.addRoleOption(option => option.setName('role1').setDescription('1st role you want to add.').setRequired(true))
-		.addRoleOption(option => option.setName(`role2`).setDescription('2nd role you want to add.').setRequired(true)),
-		/*.addRoleOption(option => option.setName(`role3`).setDescription('3rd role you want to add.').setRequired(true))
-		.addRoleOption(option => option.setName(`role4`).setDescription('4th role you want to add.').setRequired(true))
-		.addRoleOption(option => option.setName(`role5`).setDescription('5th role you want to add.').setRequired(true))
-		.addRoleOption(option => option.setName(`role6`).setDescription('6th role you want to add.').setRequired(true)),*/
+		.addRoleOption(option => option.setName(`role2`).setDescription('2nd role you want to add.').setRequired(false))
+		.addRoleOption(option => option.setName(`role3`).setDescription('3nd role you want to add.').setRequired(false))
+		.addRoleOption(option => option.setName(`role4`).setDescription('4th role you want to add.').setRequired(false))
+		.addRoleOption(option => option.setName(`role5`).setDescription('5th role you want to add.').setRequired(false)),
 
 	async execute(interaction, client) {
 
 		const role1 = interaction.options.getRole('role1');
 		const role2 = interaction.options.getRole(`role2`);
-		/*const role3 = interaction.options.getRole(`role3`);
+		const role3 = interaction.options.getRole(`role3`);
 		const role4 = interaction.options.getRole(`role4`);
 		const role5 = interaction.options.getRole(`role5`);
-		const role6 = interaction.options.getRole(`role6`);*/
 		
-
-		const button = new ActionRowBuilder()
-			.addComponents(
-				new ButtonBuilder()
+		const rolesRow1 = [];
+		const rolesRow2 = [];
+		if (role1 !== null) {
+			rolesRow1.push(new ButtonBuilder()
 				.setCustomId('button1')
 				.setLabel(`${role1.name}`)
-				.setStyle(ButtonStyle.Secondary),
+				.setStyle(ButtonStyle.Secondary));
+		}
+		if (role2 !== null) {
+			rolesRow1.push(new ButtonBuilder()
+				.setCustomId('button2')
+				.setLabel(`${role2.name}`)
+				.setStyle(ButtonStyle.Secondary));
+		}
+		if (role3 !== null) {
+			rolesRow1.push(new ButtonBuilder()
+				.setCustomId('button3')
+				.setLabel(`${role3.name}`)
+				.setStyle(ButtonStyle.Secondary));
+		}
+		if (role4 !== null) {
+			rolesRow1.push(new ButtonBuilder()
+				.setCustomId('button4')
+				.setLabel(`${role4.name}`)
+				.setStyle(ButtonStyle.Secondary));
+		}
+		if (role5 !== null) {
+			rolesRow1.push(new ButtonBuilder()
+				.setCustomId('button5')
+				.setLabel(`${role5.name}`)
+				.setStyle(ButtonStyle.Secondary));
+		}
 
-            	 new ButtonBuilder()
-                .setCustomId(`button2`)
-                .setLabel(`${role2.name}`)
-                .setStyle(ButtonStyle.Secondary),
-
-        /*        new ButtonBuilder()
-                .setCustomId(`button3`)
-                .setLabel(`${role3.name}`)
-                .setStyle(ButtonStyle.Secondary),
-                
-                new ButtonBuilder()
-                .setCustomId(`button4`)
-                .setLabel(`${role4.name}`)
-                .setStyle(ButtonStyle.Secondary),
-                
-                new ButtonBuilder()
-                .setCustomId(`button5`)
-                .setLabel(`${role5.name}`)
-                .setStyle(ButtonStyle.Secondary), */
-            
-			);
-		/* const button2 = new ActionRowBuilder()
-				.addComponents (
-					new ButtonBuilder()
-					.setCustomId('button6')
-					.setLabel(`${role6.name}`)
-					.setStyle(ButtonStyle.Secondary),
-				) 
-					*/
+		const buttonRow1 = new ActionRowBuilder()
+			.addComponents(rolesRow1);
 		const embed = new EmbedBuilder()
 			.setTitle('Course Selection Tutorial')
             .setDescription('Read the steps carefully to ensure you that you get into the right class that you are registered for.')
@@ -79,7 +75,7 @@ module.exports = {
 					value: 'Verify that the channels you need access to were added to the sidebar after selecting the classes you need. You will see the left sidebar populate with channels after you click the buttons that are the classes you need.',
 				},
 			])
-		await interaction.reply({ embeds: [embed], components: [button] });
+		await interaction.reply({ embeds: [embed], components: [buttonRow1] });
 
 		const collector = await interaction.channel.createMessageComponentCollector();
 
@@ -92,7 +88,7 @@ module.exports = {
 			}else if (i.customId === 'button2') {
 				member.roles.add(role2);
 				i.reply({ content: 'Role added', ephemeral:true });
-			}/*else if (i.customId === 'button3') {
+			}else if (i.customId === 'button3') {
 				member.roles.add(role3);
 				i.reply({ content: 'Role added', ephemeral:true });
 			}else if (i.customId === 'button4') {
@@ -104,9 +100,9 @@ module.exports = {
 			}else if (i.customId === 'button6') {
 				member.roles.add(role6);
 				i.reply({ content: 'Role added', ephemeral:true });
-			}*/
+			}
 			else {
-				i.reply({ content: 'nothing happened', ephemeral:true });
+				i.reply({ content: 'nothing happened (this is an error)', ephemeral:true });
 			}
 
 		});
